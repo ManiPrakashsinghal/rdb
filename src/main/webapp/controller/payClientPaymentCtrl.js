3c4bf1ff7003d35086993014abc1843aba35f2fb
vishal <vishal@DESKTOP-GHJIDRU>
2019-06-15 09:59:48 +0530

    add grid in pay client payment

--- src/main/webapp/controller/payClientPaymentCtrl.js
+++ src/main/webapp/controller/payClientPaymentCtrl.js
@@ -2,5 +2,22 @@ app.controller('payClientPaymentCtrl', ['$scope', '$state', function($scope, $st
 	$scope.setViewDetailsTab = function(){
 		$state.go('payment');
 	};
-	
+	$scope.users = [
+{ name: "Suresh Dasari", age: 30, location: 'Chennai' },
+{ name: "Rohini Alavala", age: 29, location: 'Chennai' },
+{ name: "Suresh Dasari", age: 30, location: 'Chennai' },
+{ name: "Rohini Alavala", age: 29, location: 'Chennai' },
+{ name: "Suresh Dasari", age: 30, location: 'Chennai' },
+{ name: "Rohini Alavala", age: 29, location: 'Chennai' },
+{ name: "Suresh Dasari", age: 30, location: 'Chennai' },
+{ name: "Rohini Alavala", age: 29, location: 'Chennai' },
+{ name: "Suresh Dasari", age: 30, location: 'Chennai' },
+{ name: "Rohini Alavala", age: 29, location: 'Chennai' },
+{ name: "Suresh Dasari", age: 30, location: 'Chennai' },
+{ name: "Rohini Alavala", age: 29, location: 'Chennai' },
+{ name: "Suresh Dasari", age: 30, location: 'Chennai' },
+{ name: "Rohini Alavala", age: 29, location: 'Chennai' },
+{ name: "Suresh Dasari", age: 30, location: 'Chennai' },
+{ name: "Rohini Alavala", age: 29, location: 'Chennai' },
+];
 }]);
