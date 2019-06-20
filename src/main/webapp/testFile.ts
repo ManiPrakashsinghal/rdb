2aea516cf1d9764a257492328094eeed909df041
t120 <t120@t120.sagdomain.local>
2019-06-20 18:23:29 +0530

    select mani

--- src/main/webapp/testFile.ts
+++ src/main/webapp/testFile.ts
@@ -1,1 +1,34 @@
-//vv
+/*<!DOCTYPE html>
+<html>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+<body>
+
+<div ng-app="anApp" ng-controller="aCtrl">
+    <select ng-init="setDefault()" ng-model="model" ng-options="k.value as k.key for  k in chooses">
+     </select>
+       
+</div>
+
+
+<script>
+var app = angular.module('anApp', []);
+app.controller('aCtrl', function($scope) {
+
+  $scope.chooses = [{"key":"one","value":1},{"key":"Two","value":2},{"key":"Three","value":3}];
+  
+
+  $scope.setDefault = function(){
+   
+      $scope.model = $scope.chooses[1].value;
+  }
+  
+   
+})
+</script>
+
+<p>When you use the ng-repeat directive to create dropdown lists, the selected value must be a string.</p>
+<p>In this example you will have to choose between the color or the model to be your selected value.</p>
+
+</body>
+</html>
+*/
